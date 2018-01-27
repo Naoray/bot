623060b4d0b409b18fb7d60cb4b8e907c512b0ae
Naoray <krishan.koenig@googlemail.com>
2018-01-27 18:35:53 +0100

    add /start infos

--- routes/botman.php
+++ routes/botman.php
@@ -6,8 +6,10 @@ use App\Http\Conversations\RegisterConversation;
 $botman = resolve('botman');
 
 $botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+	$bot->reply('Hello!');
 });
 
-$botman->hears('/link', BotManController::class.'@checkIfAlreadyRegistered');
-$botman->hears('/card', BotmanController::class.'@startRegisterCardConversation');
+$botman->hears('/start', function ($bot) {
+	$bot->reply(
+		'Before you use this service you have to link this messenger with the finn app! Use /link to get started or /help to see all available commands.');
+});
